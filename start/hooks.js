2bceb1c507becb4101503f7dd14c710c9dc8b400
Mava <code@maxva.de>
2021-10-18 17:49:34 +0200

    Update Adnois to use typescript

--- start/hooks.js
+++
@@ -1,6 +0,0 @@
-const { hooks } = require('@adonisjs/ignitor')
-
-hooks.after.providersBooted(() => {
-    console.log("Initializing Discord Provider!")
-    const Discordbot = use('Discord/Bot')
-})
